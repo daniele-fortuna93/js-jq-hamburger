bece606373d20ff45088825712c237ab6a1ad8d9
Daniele <bambudio@libero.it>
2021-03-11 16:06:53 +0100

    Aggiunta soluzione con la classe active

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var hamburger = $('.header-right > a'); // variabile icona hamburger
 var hamburgerMenu = $('.hamburger-menu'); // variabile menù nascosto
 var chiudiMenu = $('.close'); // variabile 'X' per la chiusura dell'hamburgerMenu
 
+
 // Al click dell'icona hamburger si apre il menù nascosto
 hamburger.click(
   function() {
@@ -12,5 +13,20 @@ hamburger.click(
 chiudiMenu.click(
   function() {
     hamburgerMenu.slideUp();
+  }
+);
+
+
+// Soluzione con l'uso della classe 'active'
+// Al click dell'icona hamburger aggiungo la classe 'active'
+hamburger.click(
+  function() {
+    hamburgerMenu.addClass('active');
+  }
+);
+// Al click della crocetta rimuovo la classe 'active'
+chiudiMenu.click(
+  function() {
+    hamburgerMenu.removeClass('active');
   }
 );
